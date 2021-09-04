4e06183cef353ae13cb7550746e612c9cc87c740
Ayden51 <dconghuy0@gmail.com>
2021-09-04 18:28:59 +0700

    Inital build

---
+++ Blog.js
@@ -0,0 +1,7 @@
+let head = document.getElementsByTagName("head")[0];
+let link = document.createElement("link");
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href =
+  "https://cdn.jsdelivr.net/gh/phosphor-icons/phosphor-home@v1.3.1/src/components/SizeInput/SizeInput.css";
+head.appendChild(link);
