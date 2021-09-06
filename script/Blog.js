04add813844a6b137f7f4dc91ce8b498f70dc2f2
Ayden51 <dconghuy0@gmail.com>
2021-09-06 14:25:44 +0700

    fix url in Blog.js and atomic style in Blog.css

--- script/Blog.js
+++ script/Blog.js
@@ -3,5 +3,5 @@ let link = document.createElement("link");
 link.rel = "stylesheet";
 link.type = "text/css";
 link.href =
-  "https://cdn.jsdelivr.net/gh/Ayden51/StudyCare-Website@0.5.0-alpha/css/Blog.min.css";
+  "https://cdn.jsdelivr.net/gh/Ayden51/StudyCare-Website@0.5.2-alpha/css/Blog.min.css";
 head.appendChild(link);
