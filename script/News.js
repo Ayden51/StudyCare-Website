aaa13c4aaa00ec3e1fd00ce864b28b8644ae7b8d
Ayden51 <dconghuy0@gmail.com>
2021-09-06 20:46:57 +0700

    Update Template and prepare for news article

---
+++ script/News.js
@@ -0,0 +1,7 @@
+let head = document.getElementsByTagName("head")[0];
+let link = document.createElement("link");
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href =
+  "https://cdn.jsdelivr.net/gh/Ayden51/StudyCare-Website@0.5.2-alpha/css/News.min.css";
+head.appendChild(link);
