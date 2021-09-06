160ffc4c1f4b09c87bdf5f951503aee9aee0a26c
Ayden51 <dconghuy0@gmail.com>
2021-09-06 13:14:53 +0700

    Build basic file structure

--- script/Blog.js
+++ script/Blog.js
@@ -3,5 +3,5 @@ let link = document.createElement("link");
 link.rel = "stylesheet";
 link.type = "text/css";
 link.href =
-  "https://cdn.jsdelivr.net/gh/Ayden51/StudyCare-Website@latest/Blog.min.css";
+  "https://cdn.jsdelivr.net/gh/Ayden51/StudyCare-Website@0.3.0/Blog.min.css";
 head.appendChild(link);
