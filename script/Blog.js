78030b8a12cef69fc7107c44c04e0591f3f04156
Ayden51 <dconghuy0@gmail.com>
2021-09-09 15:19:36 +0700

    Change MathJax style

--- script/Blog.js
+++ script/Blog.js
@@ -3,5 +3,5 @@ let link = document.createElement("link");
 link.rel = "stylesheet";
 link.type = "text/css";
 link.href =
-  "https://cdn.jsdelivr.net/gh/Ayden51/StudyCare-Website@0.6.1-alpha/css/Blog.min.css";
+  "https://cdn.jsdelivr.net/gh/Ayden51/StudyCare-Website@0.7.0/css/Blog.min.css";
 head.appendChild(link);
