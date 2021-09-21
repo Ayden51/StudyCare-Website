0e764b3dea71aae570958fafb5a3a468ebc64cb2
Ayden51 <dconghuy0@gmail.com>
2021-09-21 13:25:21 +0700

    Update Footer Template and News.css

--- script/News.js
+++ script/News.js
@@ -3,5 +3,5 @@ var link = document.createElement("link");
 link.rel = "stylesheet";
 link.type = "text/css";
 link.href =
-  "https://cdn.jsdelivr.net/gh/Ayden51/StudyCare-Website@0.14/css/News.min.css";
+  "https://cdn.jsdelivr.net/gh/Ayden51/StudyCare-Website@0.16/css/News.min.css";
 head.appendChild(link);
