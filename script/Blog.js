4205b12f549ca8505f73b55aa6b3a1680ba58ad2
Ayden51 <dconghuy0@gmail.com>
2022-02-08 22:07:22 +0700

    add width to news-table and change file names

--- script/Blog.js
+++ script/Blog.js
@@ -2,5 +2,5 @@ var head = document.getElementsByTagName("head")[0];
 var link = document.createElement("link");
 link.rel = "stylesheet";
 link.type = "text/css";
-link.href = "https://cdn.jsdelivr.net/gh/Ayden51/StudyCare-Website@0.19/css/Blog.min.css";
+link.href = "https://cdn.jsdelivr.net/gh/Ayden51/StudyCare-Website@0.22/css/blog.min.css";
 head.appendChild(link);
