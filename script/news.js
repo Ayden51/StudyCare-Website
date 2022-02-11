bb4ec4595602b97ce18fff3cb6d5af23a13dde1b
Ayden51 <dconghuy0@gmail.com>
2022-02-11 16:02:43 +0700

    Add scroll back from grammar table

--- script/news.js
+++ script/news.js
@@ -2,5 +2,20 @@ var head = document.getElementsByTagName("head")[0];
 var link = document.createElement("link");
 link.rel = "stylesheet";
 link.type = "text/css";
-link.href = "https://cdn.jsdelivr.net/gh/Ayden51/StudyCare-Website@0.22.4/css/news.min.css";
+link.href = "https://cdn.jsdelivr.net/gh/Ayden51/StudyCare-Website@0.23/css/news.min.css";
 head.appendChild(link);
+
+function grammarScrollToView() {
+  if (!!document.querySelector("#grammar-table") === false) return;
+  const tableBody = document.querySelector("tbody");
+  const tableRows = tableBody.querySelectorAll("tr");
+  tableRows.forEach((row) => {
+    row.addEventListener("click", () => {
+      let rowId = row.id;
+      let anchorSelector = `a[href^="#${rowId}"]`;
+      let grammarAnchor = document.querySelector(anchorSelector);
+      grammarAnchor.scrollIntoView();
+    });
+  });
+}
+grammarScrollToView();
